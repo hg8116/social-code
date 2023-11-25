d0bfd8b55c6c5a36c5673e65c6da82bc4646efee
hg8116 <2011.harshgupta@gmail.com>
2023-11-25 16:47:54 +0530

    update

--- src/Components/CodeBox.tsx
+++ src/Components/CodeBox.tsx
@@ -1,41 +1,25 @@
-import { FC, useCallback, useState, useRef } from "react"
+import { FC, useCallback, useState } from "react"
 import CodeMirror, { Extension } from "@uiw/react-codemirror"
 import { javascript } from "@codemirror/lang-javascript"
 import { themes } from "../utils/Themes"
 import EditorSettings from "./EditorSettings"
-import html2canvas from "html2canvas"
 
-const CodeBox: FC = () => {
-  const [value, setValue] = useState<string>('console.log("hello world!")')
+interface CodeBoxProps {
+  printRef: React.RefObject<HTMLDivElement>
+}
+
+const CodeBox: FC<CodeBoxProps> = ({printRef}) => {
+  const [value, setValue] = useState<string>('// Code here')
   const [editorTheme, setEditorTheme] = useState<Extension>(themes[0].libName)
   const [fontSize, setFontSize] = useState<string>("16px")
   const [height, setHeight] = useState<string>("150px")
   const [width, setWidth] = useState<string>("300px")
   const [fontFamily, setFontFamily] = useState<string>("monospace")
 
-  const printRef = useRef<HTMLDivElement>(null)
-
   const onChange = useCallback((val: string): void => {
     setValue(val)
   }, [])
 
-  const handleDownloadImage = async () => {
-    const element = printRef.current;
-    if(!element) return
-    const canvas = await html2canvas(element)
-    const data = canvas.toDataURL('image/png')
-    const link = document.createElement('a')
-    if(typeof link.download === 'string'){
-      link.href = data
-      link.download = 'img.jpg'
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    }
-    else{
-      window.open(data)
-    }
-  }
 
   return (
     <div>
@@ -51,7 +35,6 @@ const CodeBox: FC = () => {
         onWidthChange={setWidth}
         onFontFamilyChange={setFontFamily}
       />
-      <button type="button" onClick={handleDownloadImage}>Download Img</button>
       <div ref={printRef}>
       <CodeMirror
         value={value}
@@ -60,7 +43,7 @@ const CodeBox: FC = () => {
         minWidth={width}
         extensions={[javascript({ jsx: true })]}
         onChange={onChange}
-        style={{ fontFamily: fontFamily, fontSize: fontSize }}
+        style={{ fontSize: fontSize, fontFamily: fontFamily}}
       />
       </div>
     </div>
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,12 +1,35 @@
-import { FC } from "react"
+import { FC, useRef } from "react"
 import CodeBox from "./CodeBox"
+import html2canvas from "html2canvas"
 
 const Hero: FC = () => {
+  const printRef = useRef<HTMLDivElement>(null)
+
+  const handleDownloadImage = async () => {
+    const element = printRef.current;
+    if(!element) return
+    const canvas = await html2canvas(element)
+    const data = canvas.toDataURL('image/png')
+    const link = document.createElement('a')
+    if(typeof link.download === 'string'){
+      link.href = data
+      link.download = 'img.jpg'
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    }
+    else{
+      window.open(data)
+    }
+  }
+
+
   return (
-    <div className="flex justify-center items-center flex-grow">
+    <div className="flex flex-col justify-center items-center flex-grow">
       <div className="border-2 border-dashed border-blue-800 bg-gray-200 flex justify-center items-center px-20 py-10">
-        <CodeBox />
+        <CodeBox printRef={printRef}/>
       </div>
+      <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleDownloadImage}>Download Image</button>
     </div>
   )
 }
